0e8239ac593eb7c7b89ecc7ef4ceb3be3eabc08e
Alex56390 <lebrunalex7@gmail.com>
2023-06-28 11:13:53 +0200

    Commit

--- js/router.js
+++ js/router.js
@@ -3,14 +3,45 @@
  */
 
 const routeConfig = {
-  routes: [],
+  routes: [
+    {
+        path:'/thumbnail',
+        initialisation:undefined,
+        templateUrl:'/view/thumbnail.html'
+    },
+    {
+        path:'/',
+        initialisation:undefined,
+        templateUrl:'/view/html.html'
+    },
+    {
+        path:'/break',
+        initialisation:undefined,
+        templateUrl:'/view/templateQuiExistePasSurLeServeur.html'
+    }
+  ],
 };
 
 /**
  * fonction de maintient de la route
  */
-export const handleRoute = () => {
-  const pathName = location.pathname;
-  console.log(pathName);
-};
 
+class Router{
+    /**
+     * manage la route en cours
+     */
+    handleRoute() {
+        const pathName = location.pathname;
+        console.log(pathName);
+    };
+    /**
+     * Navigate to
+     * @param {string} pathname chemin commençant par /
+     */
+    changeRoute(pathname){
+
+    }
+}
+const router = new Router();
+router.handleRoute();
+router.changeRoute();
